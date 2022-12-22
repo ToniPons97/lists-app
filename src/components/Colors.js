ee8f090a5e19e19d3e56bba83d7f3fe41e24eec3
Antonio Pons <toninopons@gmail.com>
2022-12-22 09:35:04 +0100

    First commit.

---
+++ src/components/Colors.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Colors extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map((item, index) => <li key={item+index}>{item}</li>)}
+            </ul>
+        );
+    }
+}
