0f6625692b777f96fbc3837887fb2a5a23a8c526
Antonio Pons <toninopons@gmail.com>
2023-01-01 13:49:04 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
 
-function App() {
-  return (
-    <div className='flex flex-row justify-center items-center'>
-      <TodoList />
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    return (
+      <div className='flex flex-row justify-center items-center'>
+        <TodoList render={(items, handleItemDeletion, styles) => {
+          return (
+            <ul>
+              {
+                items.map((item) => 
+                {
+                  return (
+                      <li key={crypto.randomUUID()} className={styles.items}>
+                          <span className={styles.itemTxt}>{item}</span>
+                          <button className={styles.redBtn} value={item} onClick={handleItemDeletion}>Done</button>
+                      </li>
+                  );
+                })
+              }
+            </ul>
+          )
+
+        }}/>
+      </div>
+    );
+  }
+
 
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ export class TodoList extends React.Component {
     
     state = {
         items: ['Buy bread', 'Milk the cow', 'Water the plants', 'Prepare tuna salad']
-    };
+    }
 
     addTodoItem = () => {
         const input = this._inputRef.current;
@@ -32,46 +32,37 @@ export class TodoList extends React.Component {
         }
     }
 
-    handleItemDeletion = (event) => {
-        this.setState({items: this.state.items.filter(item => item !== event.target.value)});
-    }
-
+    handleItemDeletion = (event) => this.setState({items: this.state.items.filter(item => item !== event.target.value)});
 
     render() {
-        const redButtonStyles = `text-red-600 text-sm bg-gray-100 border-black border-solid 
-            border-2 h-30 w-16 rounded-md p-1 transition ease-in-out m-0 hover:bg-red-200 hover:text-white`;
-        const itemsStyles = "flex flex-col flex-wrap gap-1 text-white-400 border-black border-solid border-2 w-28 m-4 p-2";
+        const styles = {
+            greenBtn: `text-green-600 text-sm bg-gray-100 border-black border-solid 
+                border-2 rounded-md p-1 transition ease-in-out m-0 hover:bg-sky-200 hover:text-white`,
+            redBtn: `text-red-600 text-sm bg-gray-100 border-black border-solid 
+                border-2 h-30 w-16 rounded-md p-1 transition ease-in-out m-0 hover:bg-red-200 hover:text-white`,
+            items: 'flex flex-col flex-wrap gap-1 text-white-400 border-black border-solid border-2 w-28 m-4 p-2',
+            input: `mt-4 form-control w-full text-base text-gray-500 bg-white bg-clip-padding 
+                border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-400 focus:bg-white 
+                focus:border-blue-600 focus:outline-none`,
+            container: 'p-4 bg-green-100 flex flex-col items-center justify-center',
+            noTodos: 'mt-4 text-red-600 font-medium',
+            itemTxt: 'text-gray-500',
+            addRemoveContainer: 'flex flex-row justify-center items-center gap-3'
+        }
         
-        const inputStyle = `form-control w-full text-base text-gray-500 bg-white bg-clip-padding 
-            border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-400 focus:bg-white 
-            focus:border-blue-600 focus:outline-none`;
-
-        const greenButtonStyle = `text-green-600 text-sm bg-gray-100 border-black border-solid 
-            border-2 rounded-md p-1 transition ease-in-out m-0 hover:bg-sky-200 hover:text-white`;
         return (
-            <div className="p-4 bg-green-100 flex flex-col items-center justify-center">
+            <div className={styles.container}>
                 <h1 className="font-bold underline">Todo List</h1>
-                <input className={inputStyle} ref={this._inputRef} placeholder="Example: buy milk." autoComplete="off" />
+                <input className={styles.input} ref={this._inputRef} placeholder="Example: buy milk." autoComplete="off" />
                 <br />
-                <div className="flex flex-row justify-center items-center gap-3">
-                    <button className={greenButtonStyle} onClick={this.addTodoItem}>Add todo!</button>
-                    <button className={redButtonStyles} onClick={this.handleListReset}>Reset</button>
+                <div className={styles.addRemoveContainer}>
+                    <button className={styles.greenBtn} onClick={this.addTodoItem}>Add todo!</button>
+                    <button className={styles.redBtn} onClick={this.handleListReset}>Reset</button>
                 </div>
-
-                <ul>
                     {
-                        this.state.items.map((item, index) => 
-                        {
-                            return (
-                                <li key={item+index} className={itemsStyles}>
-                                    <span className="text-gray-500">{item}</span>
-                                    <button className={redButtonStyles} value={item} onClick={this.handleItemDeletion}>Done</button>
-                                </li>
-                            );
-                        })
+                        this.props.render(this.state.items, this.handleItemDeletion, styles)
                     }
-                </ul>
-                    {!!(this.state.items.length === 0)&& <h4 className="text-red-600 font-medium">No todos found!</h4>}
+                    {!!(this.state.items.length === 0)&& <h4 className={styles.noTodos}>No todos found!</h4>}
             </div>
         );
     }
